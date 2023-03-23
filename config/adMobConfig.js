381dbc67bb8206f71ed4d752f47f31146516f903
Pedro Goncal <bpg.pedro@gmail.com>
2023-03-23 08:59:44 -0600

    iOs configurations for deployment

--- config/adMobConfig.js
+++ config/adMobConfig.js
@@ -1,5 +1,5 @@
 import mobileAds, { TestIds} from 'react-native-google-mobile-ads';
 
 
-export const AD_UNIT_ID = __DEV__ ? TestIds.BANNER : 'ca-app-pub-8580824143358427~8104395596';
-export const AD_UNIT_INTERSTITIAL_ID = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
+export const AD_UNIT_ID = __DEV__ ? TestIds.BANNER : 'ca-app-pub-8580824143358427/2629075195';
+export const AD_UNIT_INTERSTITIAL_ID = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-8580824143358427/4105808393';
